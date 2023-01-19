0a36acef3021c814acaf68e8a3ed6fea48659cd0
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2023-01-19 13:00:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import os
 
 class grafos:
 
-    def __init__(self, dados=None, dicionario_grafo=None):  # Sendo none não é obrigadtório passalos ao fazer uma instancia
+    def __init__(self, dados=None, dicionario_grafo=None, vertices=None):  # Sendo none não é obrigadtório passalos ao fazer uma instancia
         if dicionario_grafo is None:
             dicionario_grafo = {}
             dados = []
         self.dicionario_grafo = dicionario_grafo
         self.dados = dados
+        self.vertices = vertices
 
     def import_graph(self, modo=None):
         self.modo = modo
@@ -24,7 +25,6 @@ class grafos:
         del (self.dados[0])
 
         if self.modo == 'undirected':
-            print('salve')
             for i in range(0, len(self.dados) - 1):
                 self.dicionario_grafo[self.dados[i][0]] = list()
                 self.dicionario_grafo[self.dados[i][2]] = list()
@@ -33,7 +33,6 @@ class grafos:
                 if i not in self.dicionario_grafo:
                     self.dicionario_grafo[self.dados[i][0]].append(self.dados[i][2])
                     self.dicionario_grafo[self.dados[i][2]].append(self.dados[i][0])
-            print(self.dicionario_grafo)
 
         elif self.modo == 'directed':
 
@@ -44,13 +43,28 @@ class grafos:
             for i in range(0, len(self.dados)): #foi ajustado não estava iterando commpletamente
                 self.dicionario_grafo[self.dados[i][0]].append(self.dados[i][2])
 
-        return self.dados
+        return self.dicionario_grafo
 
     def mostra_grafo(self):
         for k, v in self.dicionario_grafo.items():
             print(f'{k}: {v}')
 
-g = grafos()
-g.import_graph()
-g.mostra_grafo()
+    def mostra_matriz_adjacencia(self, vertices, matriz=None):
+        self.vertices = vertices
+        self.matriz = [[0] * self.vertices for i in range(self.vertices)]  # criação da matriz que só tem zero
+        for i in range(self.vertices):
+            print(self.matriz[i])
+        #OBS será implementada a isnerção das arestas na matriz
 
+
+#g = grafos()
+#d =g.import_graph()
+#g.mostra_grafo()
+#g.mostra_matriz_adjacencia(len(d))
+#conversão de letras em números, creio que de para utilizar
+
+# l = "a b c"
+# n = []
+# for x in l:
+#    n.append(ord(x) - 96)
+# print(n)

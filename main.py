3ad8f3d595dfdf9d46541cfc8380c4b138f73c7f
Gabriel Matos <73392609+GGMmattos@users.noreply.github.com>
2023-01-20 01:05:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 
 class grafos:
 
-    def __init__(self, dados=None, dicionario_grafo=None, vertices=None):  # Sendo none não é obrigadtório passalos ao fazer uma instancia
+    def __init__(self, dados=None, dicionario_grafo=None, vertices=None, directed=False):  # Sendo none não é obrigadtório passalos ao fazer uma instancia
         if dicionario_grafo is None:
             dicionario_grafo = {}
             dados = []
         self.dicionario_grafo = dicionario_grafo
         self.dados = dados
         self.vertices = vertices
+        self.directed = directed
 
     def import_graph(self, modo=None):
         self.modo = modo
@@ -34,7 +35,7 @@ class grafos:
                     self.dicionario_grafo[self.dados[i][2]].append(self.dados[i][0])
 
         elif self.modo == 'directed':
-
+            self.directed = True
             for i in range(0, len(self.dados)): #foi ajustado não estava iterando commpletamente
                 self.dicionario_grafo[self.dados[i][0]] = list()
                 self.dicionario_grafo[self.dados[i][2]] = list()
@@ -55,13 +56,60 @@ class grafos:
             print(self.matriz[i])
         #OBS será implementada a inserção das arestas na matriz
 
+    def inserir_vertice(self, vertice):
+        self.vertices = vertice
+        if self.vertices not in self.dicionario_grafo:
+            key = self.vertices
+            self.dicionario_grafo[key] = list()
+            print(f'O vertice {self.vertices} foi adicionado!!! ')
+        else:
+            print(f'O vertice {self.vertices} já faz parte do grafo!!!')
 
-#g = grafos()
-#d = g.import_graph()
-#g.mostra_grafo()
+    def inserir_aresta(self, v1, v2):
+        self.v1 = v1
+        self.v2 = v2
+
+        #Para grafos não direcionados
+        if self.v1 not in self.dicionario_grafo:
+            print(f'{self.v1} não faz parte do grafo!!!')
+
+        elif self.v2 not in self.dicionario_grafo:
+            print(f'{self.v2} não faz parte do grafo!!!')
+
+        elif self.v2 in self.dicionario_grafo[self.v1]:
+            print('Aresta ja exite!!!')
+        else:
+            if self.directed:
+                self.dicionario_grafo[self.v1].append(self.v2)
+            else:
+                self.dicionario_grafo[self.v1].append(self.v2)
+                self.dicionario_grafo[self.v2].append(self.v1)
+
+    def remover_vertice(self, vertice):
+        self.dicionario_grafo.pop(vertice)
+
+    def remover_aresta(self, ):
+        pass
+
+
+g = grafos()
+d = g.import_graph()
+g.mostra_grafo()
+# g.inserir_vertice('f')
+# g.inserir_vertice('g')
+# g.inserir_vertice('f')
+# g.inserir_aresta('a', 'f')
+# g.inserir_aresta('a', 'g')
+g.inserir_aresta('c', 'a')
+print(' ')
+g.remover_vertice('a')
+g.mostra_grafo()
+
+
+
 #g.mostra_matriz_adjacencia(len(d))
-#conversão de letras em números, creio que de para utilizar
 
+#conversão de letras em números, creio que de para utilizar
 # l = "a b c"
 # n = []
 # for x in l:
